c56962c5f7154f1c5f9c7e0ef6cba27039ff5067
RomanWillems <90450945+RomanWillems@users.noreply.github.com>
2021-09-10 13:32:58 +0200

    Eerste gesprekje

--- python_basics/helloworld.py
+++ python_basics/helloworld.py
@@ -1,2 +1,6 @@
-print("hello world")
 
+print('hallo meneer, wat is jou naam?')
+username = input ("")
+print("fakka",username, "leuk je te ontmoeten. Hoe oud ben je?")
+leeftijd = input("")
+print(leeftijd, "pas, nog een groentje dus")
