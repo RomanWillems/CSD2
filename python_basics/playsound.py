8928e6cc740efd649b97fae6fb8a2fac57bb0ccf
RomanWillems <90450945+RomanWillems@users.noreply.github.com>
2021-09-13 13:02:35 +0200

    Geluid sample afspelen

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,3 +1,11 @@
-import simpleaudio.functionchecks as fc
+# import simpleaudio.functionchecks as fc
 
-fc.LeftRightCheck.run()
+# fc.LeftRightCheck.run()
+
+import simpleaudio as sa
+
+filename = 'fieuw.wav'
+wave_obj = sa.WaveObject.from_wave_file(filename)
+play_obj = wave_obj.play()
+play_obj.wait_done()
+
