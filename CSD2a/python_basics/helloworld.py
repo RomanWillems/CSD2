05efae641d613edd3a819532fd3408f7e3b46c19
RomanWillems <90450945+RomanWillems@users.noreply.github.com>
2021-09-16 22:30:57 +0200

    Kick afspelen naar wens

--- CSD2a/python_basics/helloworld.py
+++ CSD2a/python_basics/helloworld.py
@@ -1,6 +1,11 @@
 
-print('hallo meneer, wat is jou naam?')
-username = input ("")
-print("fakka",username, "leuk je te ontmoeten. Hoe oud ben je?")
-leeftijd = input("")
-print(leeftijd, "pas, nog een groentje dus")
+
+def main():
+    print('hallo meneer, wat is jou naam?')
+    username = input ("")
+    print("jo",username, "leuk je te ontmoeten. Hoe oud ben je?")
+    leeftijd = input("")
+    print(leeftijd, "pas, nog een groentje dus") 
+
+if __name__ == "__main__":
+    main()
