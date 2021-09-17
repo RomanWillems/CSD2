8f5311e466e8f8cf1555c42b7433a4db5b8fd171
RomanWillems <90450945+RomanWillems@users.noreply.github.com>
2021-09-17 10:42:52 +0200

    ritmishe playback met bpm en notelengte

--- CSD2a/python_basics/playsound.py
+++ CSD2a/python_basics/playsound.py
@@ -2,27 +2,27 @@
 
 #  fc.LeftRightCheck.run()
 
-import simpleaudio as sa
-
-print('how many times do you want to play the sample?')
-aantalSpelen = input ("")
-
-for i in range(1, aantalSpelen + 1) :
-    filename = 'Kick.wav'
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-
-
-# Howmuch = ["1","2","3","4","5"]
-# for x in Howmuch:
-#    play_obj = wave_obj.play()
-
 # import simpleaudio as sa
 
-# filename = 'fieuw.wav'
-# wave_obj = sa.WaveObject.from_wave_file(filename)
-# play_obj = wave_obj.play()
-# play_obj.wait_done()
+# print('how many times do you want to play the sample?')
+# aantalSpelen = input ("")
+
+# for i in range(1, aantalSpelen + 1) :
+#     filename = 'Kick.wav'
+#     wave_obj = sa.WaveObject.from_wave_file(filename)
+#     play_obj = wave_obj.play()
+#     play_obj.wait_done()
+
+
+Howmuch = ["1","2","3","4","5"]
+for x in Howmuch:
+   play_obj = wave_obj.play()
+
+import simpleaudio as sa
+
+filename = 'fieuw.wav'
+wave_obj = sa.WaveObject.from_wave_file(filename)
+play_obj = wave_obj.play()
+play_obj.wait_done()
 
 
