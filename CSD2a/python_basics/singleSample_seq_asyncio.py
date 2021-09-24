d2c7a4385ec79bfa3d0ea0cd6d97228c5bbff39a
RomanWillems <90450945+RomanWillems@users.noreply.github.com>
2021-09-24 10:36:30 +0200

    converted noteTimes to 16th time stamps

--- CSD2a/python_basics/singleSample_seq_asyncio.py
+++ CSD2a/python_basics/singleSample_seq_asyncio.py
@@ -2,7 +2,6 @@ import simpleaudio as sa
 import asyncio
 
 #load a sample.
-
 print("choose your sample!")
 print("kick type: 1")
 print("flute type: 2")
@@ -29,12 +28,25 @@ if wichBpm == "no":
 print("bpm=", bpmInput)
 
 
-#add the note times given in a array in a certain order
+#add the note times given in a list in the correct order
 print('Select the note times, 1 = 1, 1/2 = 0.5, 1/4 = 0.25')
 noteTimes = []
 for i in range(playCount):
     noteTimes.append(float(input("")))
 print("note times =", noteTimes)
+
+#convert the noteTimes to 16th time stamps
+timestamps16 = []
+latestVal = 0
+for i in noteTimes:
+    timestamps16.append(latestVal)
+    if i == 0.25:
+        latestVal += 1
+    elif i == 0.50:
+        latestVal += 2
+    elif i == 1:
+        latestVal += 4
+print("timestamps in 16th=", timestamps16)
 
 #get sample
 sample = sa.WaveObject.from_wave_file(filename)
