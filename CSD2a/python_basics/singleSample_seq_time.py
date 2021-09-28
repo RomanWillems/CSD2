8a46c532af92c36307cebfd5cfb24fc82c7b3936
RomanWillems <90450945+RomanWillems@users.noreply.github.com>
2021-09-28 13:21:43 +0200

    wacht tot de laatste noot is afgespeeld

--- CSD2a/python_basics/singleSample_seq_time.py
+++ CSD2a/python_basics/singleSample_seq_time.py
@@ -1,4 +1,5 @@
 
+#import functions
 import simpleaudio as sa
 import time
 
@@ -14,14 +15,14 @@ if sampleChoise == "2":
  
 #ask en get the playTimes
 print('how many times do you want to play the sample?')
-playCount = int(input (""))
+playCount = int(input(""))
 
 #define a quarternote in ms according to the bpm
 print('the default bpm is 120, do you want to change it? type: yes or no')
 wichBpm = input("")
 if wichBpm == "yes":
     print("type your favorite bpm!")
-    bpmInput = int(input (""))
+    bpmInput = int(input(""))
     bpm_ms = 60 / bpmInput
 if wichBpm == "no":
     bpmInput = 120
@@ -32,6 +33,7 @@ print("bpm=", bpmInput)
 print('Select the note times, 1 = 1, 1/2 = 0.5, 1/4 = 0.25')
 noteTimes = []
 for i in range(playCount):
+    print("duration", i)
     noteTimes.append(float(input("")))
 print("note times =", noteTimes)
 
@@ -48,55 +50,36 @@ for i in noteTimes:
         latestVal += 4
 print("timestamps in 16th=", timestamps16)
 
+#convert the timestamps16th to time durations in ms
 timeStamps = []
 time16thNote = bpm_ms / 4
-
 for i in timestamps16:
     timeStamps.append(i * time16thNote)
 print("timesStamps=", timeStamps)
 
 
- #get sample
+#get sample
 sample = sa.WaveObject.from_wave_file(filename)
+
+#Get starting point
 time_zero = time.time()
 
+#Get the first length form the timeStamps list and store it in ts
 if timeStamps:
     ts = timeStamps.pop(0)
+sleepTime = timeStamps[-1]
 
+#Start time counter and play if the time >= then the ts
 while True:
     now = time.time() - time_zero
-    for i in timeStamps:
-        if (now >= ts):
-             sample.play()
-             if timeStamps:
-                 ts = timeStamps.pop(0)
-
+    if (now >= ts):
+        sample.play()
+        #Get the next length en store it in ts again
+        if timeStamps:
+            ts = timeStamps.pop(0)
+        else:
+            break
     time.sleep(0.001)
-    
-time.sleep(time16thNote[-1])
 
-
-
-
-
-
-
-
-
-
-#sequence the loop and play the samples
-# noteLength = []
-# for i in noteTimes:
-#     noteLength.append(bpm_ms * i)
-# print("notelengte=", noteLength)
-
-
-# #calculate current
-
-# time_sum = 0
-
-# # while i < (1 + playCount):
-
-
-
+time.sleep(sleepTime)
     
