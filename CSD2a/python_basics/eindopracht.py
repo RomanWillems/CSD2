c26870e54732aec949948e5e46c59b621fe30e86
RomanWillems <90450945+RomanWillems@users.noreply.github.com>
2021-10-12 16:28:02 +0200

    example ciska

--- CSD2a/python_basics/eindopracht.py
+++ CSD2a/python_basics/eindopracht.py
@@ -34,6 +34,7 @@ def event_stamps(time_sig,place):
         "place": place,
     }
 
+
 #get the wished time signatures
 print("choose time signature")
 print('(4) = 4/4')
@@ -43,27 +44,38 @@ print('(5) = 5/4')
 #convert the choosen time signature to possible places on the grid
 choosen_time_signature = input("")
 if choosen_time_signature == "4":
-    time_signature = [1,2,3,4,5,6,7,8]
+    #make a list of probabilitys per instrument
+    #8 numbers is 4/4
+    probability_kick = [100,50,25,50,85,50,25,50]
+    probability_snare = [0,25,50,50,80,25,50,50]
+    probability_hihat = [25,50,75,50,25,50,75,50]
 if choosen_time_signature == "3":
-    time_signature = [1,2,3,4,5,6]
+    #6 numbers is 3/4
+    probability_kick = [100,50,25,80,25,50]
+    probability_snare = [0,25,50,80,30,50]
+    probability_hihat = [25,75,50,25,75,50]
 if choosen_time_signature == "5":
-    time_signature = [1,2,3,4,5,6,7,8,9,10]
+    #10 numbers is 5/4
+    probability_kick = [100,25,50,50,25,100,25,50,50,25]
+    probability_snare = [0,25,50,50,20,85,25,50,35,20]
+    probability_hihat = [25,50,75,50,35,25,65,75,50,35]
 
 #make a list of stamps for each individual instrument
+#stamp is randomly generated accoriding to the probability in the probability lists
 stamps = []
 def random_place():
-    for i in time_signature:
+    for index, item in enumerate(probability_kick):
         rand = random.randint(1,100)
-        if (rand <= 25):
-            stamps.append(event_stamps("stamp_kick",i))
-    for i in time_signature:
+        if (rand <= item):
+            stamps.append(event_stamps("stamp_kick",index))
+    for index, item in enumerate(probability_snare):
         rand1 = random.randint(1,100)
-        if (rand1 <= 30):
-            stamps.append(event_stamps("stamp_snare",i))
-    for i in time_signature:
+        if (rand1 <= item):
+            stamps.append(event_stamps("stamp_snare",index))
+    for index, item in enumerate(probability_hihat):
         rand2 = random.randint(1,100)
-        if (rand2 <= 70):
-            stamps.append(event_stamps("stamp_hihat",i))
+        if (rand2 <= item):
+            stamps.append(event_stamps("stamp_hihat",index))
             
 #convert the individual stamps to timestamps in ms and collect them all together
 timeStamps = []
@@ -86,28 +98,38 @@ copyStamps = timeStamps.copy()
 
 #get starting point
 time_zero = time.time()
+
 #start the loop
 while True:
     now = time.time() - time_zero
     for i in timeStamps:
         #if the time matches a timeStamps, play the sample attached to it (kick,snare or hihat)
         if (now >= i["timestamps"]):
-            print(",,,,")
             if i["instrument"] == 'Kick':
                 Kick.play()
+                print("kick")
             if i['instrument'] == 'Snare':
                 Snare.play()
+                print("                         snare")
             if i['instrument'] == 'HiHat':
                 HiHat.play()
+                print("                                                         hihat")
 
             #Remove the timestamps when it played.
             timeStamps.remove(i)
 
             #if the timeStamps are empty, fill it with a copy of the loop so it keeps looping.
             if timeStamps == []:
-                print("....")
+                print("---------")
                 time_zero = time.time()
                 now = time.time() - time_zero
                 timeStamps = copyStamps
                 copyStamps = timeStamps.copy()
+
+
+
+                    
+                
+
+    
         
