5c256523862e3b442cb621cd0ebeb751146070f3
Roman Willems <romanw@hotmail.com>
2022-04-25 15:02:00 +0200

    DFT test aplitude in frequency domain plotting tests

--- CSD2D/Python_Testjes/fft_test.py
+++
@@ -1,6 +0,0 @@
-# Link to this test, plotting waves.
-# https://pythonnumericalmethods.berkeley.edu/notebooks/chapter24.01-The-Basics-of-waves.html
-
-import simpleaudio.functionchecks as fc
-
-fc.LeftRightCheck.run()
