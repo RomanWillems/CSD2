b3d76baf12b81af25db4feef121550ee97ce1ecd
Roman Willems <romanw@hotmail.com>
2022-04-25 13:47:07 +0200

    FFT map structure

---
+++ CSD2D/Python_Testjes/fft_test.py
@@ -0,0 +1,6 @@
+# Link to this test, plotting waves.
+# https://pythonnumericalmethods.berkeley.edu/notebooks/chapter24.01-The-Basics-of-waves.html
+
+import simpleaudio.functionchecks as fc
+
+fc.LeftRightCheck.run()
